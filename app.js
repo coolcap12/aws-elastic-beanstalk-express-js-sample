e46a219403a66a252fc7b02310df1a1491b0706f
Rashad Morton <mortonsbarbecue@gmail.com>
2021-07-05 22:40:43 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Accenture!'));
+app.get('/', (req, res) => res.send('Creating my Devops pipeline Practice!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
